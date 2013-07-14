ab55cfa37d79beee4fd1f132915e049da736dc44
Alex Clark <aclark@aclark.net>
2013-07-14 16:02:55 -0400

    Add nosetests

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from setuptools import setup
 setup(
     entry_points={
         # "EntryPoint must be in 'name=module:attrs [extras]' format"
-        'console_scripts': 'install-plone',
+        'console_scripts': 'install-plone=install:install',
     },
+    install_requires=[
+        'nose',
+    ],
     name='plock',
     license='Whatever license Plone is',
 )
