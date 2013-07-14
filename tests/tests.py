23df23b5064307a1f8c4045b27d7c2c442c91d5b
Alex Clark <aclark@aclark.net>
2013-07-14 16:24:53 -0400

    Flake8

--- tests/tests.py
+++ tests/tests.py
@@ -1,16 +1,21 @@
 from nose import with_setup
 
+
 def test_fail():
     assert False
+
 
 def test_pass():
     pass
 
+
 def setup_func():
     "set up test fixtures"
 
+
 def teardown_func():
     "tear down test fixtures"
+
 
 @with_setup(setup_func, teardown_func)
 def test():
