25fb98cf40c771a69c3fde545dfdfb521581b3ea
Alex Clark <aclark@aclark.net>
2013-07-14 10:06:06 -0400

    Add note

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 
 setup(
+    entry_points={
+        # "EntryPoint must be in 'name=module:attrs [extras]' format"
+        'console_scripts': 'install-plone',
+    },
     name='plock',
     license='Whatever license Plone is',
-    install_requires=[
-        'Plone',
-    ],
 )
