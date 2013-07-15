d52d928d6e1001e278dc77d5cb936cb89124c497
Alex Clark <aclark@aclark.net>
2013-07-15 18:43:02 -0400

    Add expert mode

--- plock/install.py
+++ plock/install.py
@@ -4,9 +4,15 @@ import sh
 import sys
 import time
 
+EXPERT = os.environ.get('PLOCK_EXPERT')
 
-URL = 'https://raw.github.com/pythonpackages/buildout-plone/master/4.3.x'
+CFG = 'https://raw.github.com/pythonpackages/buildout-plone/master/4.3.x'
 
+CMD1 = ('buildout:download-cache=download-cache',
+        'buildout:eggs-directory=eggs-directory',
+        'buildout:directory=.', '-U', '-c', CFG)
+
+CMD2 = ('buildout:directory=.', '-c', CFG)
 
 def create_dirs():
     dirs = ('download-cache', 'eggs-directory')
@@ -22,21 +28,20 @@ def install():
     sys.stdout.write("Installing Plone. This may take a while...")
     sys.stdout.flush()
     buildout = sh.Command("bin/buildout")
-    create_dirs()
-    download = buildout(
-        'buildout:download-cache=download-cache',
-        'buildout:eggs-directory=eggs-directory',
-        'buildout:directory=.', '-U', '-c', URL,
-        _bg=True)
-    count = 0
-    while(len(os.listdir('eggs-directory')) < 235):
-        count += 1
-        num = len(os.listdir('eggs-directory'))
-        if count % 5 == 0:
-            sys.stdout.write("(%d)" % num)
-        else:
-            time.sleep(3)
-            sys.stdout.write(".")
-        sys.stdout.flush()
-    download.wait()
+    if eval(EXPERT):
+        buildout(CMD2)
+    else:
+        create_dirs()
+        download = buildout(CMD1, _bg=True)
+        count = 0
+        while(len(os.listdir('eggs-directory')) < 235):
+            count += 1
+            num = len(os.listdir('eggs-directory'))
+            if count % 5 == 0:
+                sys.stdout.write("(%d)" % num)
+            else:
+                time.sleep(3)
+                sys.stdout.write(".")
+            sys.stdout.flush()
+        download.wait()
     print(" done.")
