de557d5ef60f7d3dc6efd289513c5ff16c40be6d
Alex Clark <aclark@aclark.net>
2013-07-21 20:00:03 -0400

    Add file(s)

---
+++ plock/config.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+
+
+ARGP = argparse.ArgumentParser(
+    description="Plock is a Plone Installer for the Pip-Loving Crowd")
+
+ARGP.add_argument(
+    "-l", "--list-addons", action="store_true", help="List add-ons from PyPI")
+
+EXPERT = os.environ.get('PLOCK_EXPERT')
+
+try:
+    EXPERT = eval(EXPERT)
+except:
+    EXPERT = False
+
+CFG = """\
+[buildout]
+extends = https://raw.github.com/pythonpackages/buildout-plone/master/latest
+
+[plone]
+eggs +=
+# Add-ons go here e.g.:
+#    Products.PloneFormGen
+"""
+
+CMD = ('buildout:download-cache=download-cache',
+       'buildout:eggs-directory=eggs-directory',
+       '-U')
