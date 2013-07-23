66478902b11e8f4e73aec8e37dc52c5e5c947d78
Alex Clark <aclark@aclark.net>
2013-07-23 17:16:34 -0400

    Add test

--- tests/tests.py
+++ tests/tests.py
@@ -1,22 +1,9 @@
 from nose import with_setup
 
 
-def test_fail():
-    assert False
-
-
-def test_pass():
-    pass
-
-
-def setup_func():
-    "set up test fixtures"
-
-
-def teardown_func():
-    "tear down test fixtures"
-
-
-@with_setup(setup_func, teardown_func)
-def test():
-    "test ..."
+def test_locale_format():
+    from plock.install import locale_format
+    import locale
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    result = locale_format(3000)
+    assert result == "3,000"
