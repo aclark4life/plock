1e9f8f7e3bfa6e11b3f2428f2ee04789cce92bad
Alex Clark <aclark@aclark.net>
2013-07-28 21:59:45 -0400

    Test args

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,12 @@
+def test_argument_parser():
+    from plock.config import argument_parser
+    import sys
+    sys.argv = ['']  # XXX Remove "test" arg passed to setup.py
+    parse_args = argument_parser.parse_args()
+    for arg in ['add_on', 'list_addons', 'preserve', 'raw', 'write_config']:
+        assert arg in parse_args
+
+
 def test_locale_format():
     from plock.install import Installer
     import locale
