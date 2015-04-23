19e240164b795050a1a7ab92fe01e975638e7ad3
Alex Clark <aclark@aclark.net>
2015-04-23 19:02:25 -0400

    Fix unified installer dir

--- plock/config.py
+++ plock/config.py
@@ -63,6 +63,6 @@ PYPI = CheeseShop()
 PYPI_OPER = 'AND'
 PYPI_SPEC = {'description': 'plone', 'keyword': 'plone', 'summary': 'plone'}
 
-UNIFIEDINSTALLER_DIR = "Plone-4.3.3-UnifiedInstaller"
+UNIFIEDINSTALLER_DIR = "Plone-4.3.4-UnifiedInstaller"
 UNIFIEDINSTALLER_URL = "https://launchpad.net/plone/4.3/4.3.4/+download/"
 UNIFIEDINSTALLER_URL += "Plone-4.3.4-r1-UnifiedInstaller.tgz"
